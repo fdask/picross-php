b982ed2f930df4115b53ffd1cdc3859b6bfc2c7e
James Wallace <sausages@gmail.com>
2024-04-13 10:12:44 -0400

    feat: Initial population of the files.

---
+++ colors.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php
+<?php
+include 'include/colors.inc';
+
+// output all our colors
+Colors::all_fg();
+Colors::all_bg();
+?>
---
+++ picto.php
@@ -0,0 +1,6 @@
+#!/usr/bin/php
+<?php
+include 'include/config.inc';
+
+$p = new Picto();
+
